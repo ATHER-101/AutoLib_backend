15832d674356ac838d010e43df7335de8ecb0790
ATHER-101 <atharvatijare04@gmail.com>
2024-07-10 00:20:29 +0530

    test5

--- routes/authentication.mjs
+++ routes/authentication.mjs
@@ -5,12 +5,14 @@ const router = Router();
 
 router.get("/api/auth/google", passport.authenticate("google"));
 
-router.get("/api/auth/google/callback", passport.authenticate("google",{
-    successRedirect: process.env.FRONTEND,
-    failureRedirect:"/api/auth/failed"
-}));
+router.get("/api/auth/google/callback", passport.authenticate("google", {
+    failureRedirect: "/api/auth/failed"
+}), (req, res) => {
+    console.log('Authentication successful, redirecting to frontend.');
+    res.redirect(process.env.FRONTEND);
+});
 
-router.get("/api/auth/logout", (request,response)=>{
+router.get("/api/auth/logout", (request, response) => {
     request.logout((error) => {
         if (error) {
             return console.log(error);
@@ -19,12 +21,19 @@ router.get("/api/auth/logout", (request,response)=>{
     });
 });
 
-router.get("/api/auth/failed",(request,response)=>{
+router.get("/api/auth/failed", (request, response) => {
     response.redirect(`${process.env.FRONTEND}/auth-failed`);
 });
 
 router.get("/api/auth/status", (request, response) => {
-    return request.user ? response.send({status:"authorised",user:request.user}) : response.status(401).send({status:"unauthorised"});
+    console.log("Session cookie:", request.headers.cookie); // Log cookie headers
+    if (request.user) {
+        console.log("User authenticated:", request.user);
+        response.send({ status: "authorised", user: request.user });
+    } else {
+        console.log("User not authenticated");
+        response.status(401).send({ status: "unauthorised" });
+    }
 });
 
 export default router;
